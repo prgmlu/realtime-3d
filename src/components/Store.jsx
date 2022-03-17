fc7d7a02a9370160beffcbde91c725cab2a81ec5
MarawanGharably <marawan@ardentpeak.com>
2022-03-17 14:09:18 +0200

    third-person-view

--- src/components/Store.jsx
+++ src/components/Store.jsx
@@ -1,17 +1,15 @@
-
 import React, { Component } from 'react'
 import * as THREE from 'three'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
-import { PointerLockControls } from './PointerLockControls'
-import wallImage from './static/wall.jpg'
-import {items, putItems} from './items';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import CharacterControls from './characterControls';
+import mainChar from './static/glb_files/Soldier.glb'
+import {items, putItems} from './items'
 
 var moveForward = false;
 var moveBackward = false;
 var moveLeft = false;
 var moveRight = false;
-
-
 
 var prevTime = performance.now();
 const velocity = new THREE.Vector3();
@@ -19,117 +17,20 @@ const direction = new THREE.Vector3();
 const raycaster = new THREE.Raycaster();
 const mouse = new THREE.Vector2();
 var objectIntersected = false;
-const lastCamPos = new THREE.Vector3();
+const lastCharPos = new THREE.Vector3();
+const camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 1, 1000 );
 
-const createStore = () => {
-	const textureLoader = new THREE.TextureLoader();
-	const newStore = new THREE.Group();
-
-	const wallGeometry = new THREE.PlaneGeometry(20, 5, 100, 100);
-	const wallTexture = textureLoader.load(wallImage);
-	const wallMaterial = new THREE.MeshStandardMaterial({ map: wallTexture, side: THREE.DoubleSide });
-	wallTexture.anisotropy = 16;
-	wallTexture.wrapS = wallTexture.wrapT = THREE.RepeatWrapping;
-	wallTexture.repeat.set(1, 1);
-	wallTexture.encoding = THREE.sRGBEncoding;
-
-	const firstWall = new THREE.Mesh(wallGeometry, wallMaterial);
-	firstWall.position.z = -10;
-	firstWall.position.y = 2.5;
-
-	const secondWall = new THREE.Mesh(wallGeometry, wallMaterial);
-	secondWall.position.z = 10;
-	secondWall.position.y = 2.5;
-
-	const thirdWall = new THREE.Mesh(wallGeometry, wallMaterial);
-	thirdWall.rotation.y = Math.PI / 2;
-	thirdWall.position.x = 10;
-	thirdWall.position.y = 2.5;
-
-	const fourthWall = new THREE.Mesh(wallGeometry, wallMaterial);
-	fourthWall.rotation.y = Math.PI / 2;
-	fourthWall.position.x = -10;
-	fourthWall.position.y = 2.5;
-
-	newStore.add(firstWall, secondWall, thirdWall, fourthWall);
-
-	return newStore;
+const initCharacter = () => {
+	const geometry = new THREE.SphereGeometry(.25, 32, 16);
+	const material = new THREE.MeshBasicMaterial({color:'red'});
+	const mainChar = new THREE.Mesh(geometry, material);
+	mainChar.position.set(0, 0.25, 0);
+	return mainChar
 }
 
-const onKeyDown = function (event) {
-
-	switch (event.code) {
-
-		case 'ArrowUp':
-		case 'KeyW':
-			moveForward = true;
-			break;
-
-		case 'ArrowLeft':
-		case 'KeyA':
-			moveLeft = true;
-			break;
-
-		case 'ArrowDown':
-		case 'KeyS':
-			moveBackward = true;
-			break;
-
-		case 'ArrowRight':
-		case 'KeyD':
-			moveRight = true;
-			break;
-
-	}
-};
-
-const onKeyUp = function (event) {
-
-	switch (event.code) {
-
-		case 'ArrowUp':
-		case 'KeyW':
-			moveForward = false;
-			break;
-
-		case 'ArrowLeft':
-		case 'KeyA':
-			moveLeft = false;
-			break;
-
-		case 'ArrowDown':
-		case 'KeyS':
-			moveBackward = false;
-			break;
-
-		case 'ArrowRight':
-		case 'KeyD':
-			moveRight = false;
-			break;
-
-	}
-};
-
-function onMouseMove(event) {
-	mouse.x = (event.clientX / document.body.innerWidth) * 2 - 1;
-	mouse.y = - (event.clientY / document.body.innerHeight) * 2 + 1;
-}
 export default class Store extends Component {
 
 	componentDidMount() {
-		const cursor = document.createElement('div')
-		cursor.style.width = '10px'
-		cursor.style.height = '10px'
-		cursor.style.background = 'red'
-		cursor.style.position = 'absolute'
-		cursor.style.top = '0'
-		cursor.style.left = '0'
-		cursor.style.right = '0'
-		cursor.style.bottom = '0'
-		cursor.style.margin = 'auto'
-		cursor.style.zIndex = '99'
-		document.body.appendChild(cursor)
-
 		const scene = new THREE.Scene();
 		const canvas = document.getElementById('webgl');
 		const renderer = new THREE.WebGLRenderer({
@@ -145,98 +46,81 @@ export default class Store extends Component {
 		scene.add(new THREE.AmbientLight(0xffffff, 0.7));
 
 		const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000);
-		camera.position.y = 1.5;
-		camera.position.z = 6;
 
-		const controls = new PointerLockControls(camera, canvas);
+		// CONTROLS
+		const orbitControls = new OrbitControls(camera, renderer.domElement);
+		orbitControls.enableDamping = true;
+		orbitControls.minDistance = 2;
+		orbitControls.maxDistance = 5;
+		orbitControls.enablePan = false;
+		orbitControls.maxPolarAngle = Math.PI / 2 - 0.05;
+		orbitControls.update();
 
-		canvas.addEventListener('click', function () { controls.lock(); });
-		document.addEventListener('keydown', onKeyDown);
-		document.addEventListener('keyup', onKeyUp);
-
-		// const surroundings = createStore();
-		// scene.add(surroundings);
-
-		scene.add(controls.getObject());
-
-
-
+		let characterControls = {};
 
 		const loader = new GLTFLoader();
 		loader.crossOrigin = true;
 
+		loader.load(mainChar, function (data) {
+
+			const model = data.scene;
+			model.traverse(function (object) {
+				if (object.isMesh) object.castShadow = true;
+			});
+
+			scene.add(model);
+
+			const charAnimations = data.animations;
+			const mixer = new THREE.AnimationMixer(model);
+			const animationsMap = new Map()
+			charAnimations.filter(a => a.name != 'TPose').forEach((a) => {
+				animationsMap.set(a.name, mixer.clipAction(a))
+			})
+
+			characterControls = new CharacterControls(model, mixer, animationsMap, orbitControls, camera,  'Idle');
+		});
+
+		// CONTROL KEYS
+		let keysPressed = {}
+		document.addEventListener('keydown', (e) => {
+			if (e.shiftKey && characterControls) {
+				characterControls.switchRunToggle()
+			} else {
+				(keysPressed)[e.key.toLowerCase()] = true
+			}
+		}, false);
+		document.addEventListener('keyup', (e) => {
+			(keysPressed)[e.key.toLowerCase()] = false
+		}, false);
+
 		putItems(scene, loader, items)
+
+		const clock = new THREE.Clock();
 
 		function animate() {
 
-			const time = performance.now();
-
-			if (controls.isLocked === true) {
-
 				// raycaster.ray.origin.copy( controls.getObject().position );
-
 				raycaster.setFromCamera(mouse, camera);
-
 				// // calculate objects intersecting the picking ray
 				// const intersects = raycaster.intersectObjects( [sphere, window.tShirt] );
-
 				// if ( intersects && intersects.length > 0) {
 				//   objectIntersected = true;
 				// }
-
 				// else{
 				//   objectIntersected = false;
 				// }
 
-
-				const delta = (time - prevTime) / 1000;
-
-				velocity.x -= velocity.x * 10.0 * delta;
-				velocity.z -= velocity.z * 10.0 * delta;
-
-				direction.z = Number(moveForward) - Number(moveBackward);
-				direction.x = Number(moveRight) - Number(moveLeft);
-				direction.normalize(); // this ensures consistent movements in all directions
-
-				if (moveForward || moveBackward) {
-
-					velocity.z -= direction.z * 30.0 * delta;
-					lastCamPos.copy(camera.position);
-				}
-				if (moveLeft || moveRight) {
-
-					velocity.x -= direction.x * 30.0 * delta;
-					lastCamPos.copy(camera.position);
-				}
-
-				controls.moveRight(- velocity.x * delta);
-				controls.moveForward(- velocity.z * delta);
-
-				if (camera.position.x < -6.5 || camera.position.x > 6.5 || camera.position.z < -6.5 || camera.position.z > 6.5) {
-
-					camera.position.copy(lastCamPos);
-				}
-
-				controls.getObject().position.y += (velocity.y * delta); // new behavior
-
-				if (controls.getObject().position.y < 1.5) {
-
-					velocity.y = 0;
-					controls.getObject().position.y = 1.5;
-
-				}
+			let mixerUpdateDelta = clock.getDelta();
+			if (characterControls.update) {
+				characterControls.update(mixerUpdateDelta, keysPressed);
 			}
 
-			prevTime = time;
-
+			orbitControls.update();
 			renderer.render(scene, camera);
-
 			requestAnimationFrame(animate);
-
 		}
 
 		animate();
-
 	}
 
 	render() {
@@ -247,5 +131,3 @@ export default class Store extends Component {
 		)
 	}
 }
-
-// export default Store;
---
+++ src/components/characterControls.jsx
@@ -0,0 +1,136 @@
+import { string } from 'prop-types';
+import React, { Component } from 'react'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+
+
+// CONSTANTS
+const fadeDuration = 0.2;
+const runVelocity = 5;
+const walkVelocity = 2;
+const DIRECTIONS = ['w', 'a', 's', 'd'];
+
+export default class CharacterControls extends Component {
+
+    constructor(model, mixer, animationsMap, orbitControl, camera, currentAction = ''){
+
+        super()
+        this.model = model
+        this.mixer = mixer
+        this.animationsMap = animationsMap
+        this.currentAction = currentAction
+        this.animationsMap.forEach((value, key) => {
+            if (key == currentAction) {
+                value.play()
+            }
+        })
+        this.orbitControl = orbitControl
+        this.camera = camera
+        this.walkDirection = new THREE.Vector3();
+        this.rotateAngle = new THREE.Vector3(0, 1, 0);
+        this.rotateQuarternion = new THREE.Quaternion();
+        this.cameraTarget = new THREE.Vector3();
+        this.updateCameraTarget(0,0)
+
+        // state
+        this.toggleRun = false;
+        this.currentAction = currentAction;
+    }
+
+    switchRunToggle(){
+        this.toggleRun = !this.toggleRun
+    }
+
+    update(delta, keysPressed){
+
+        const directionPressed = DIRECTIONS.some(key => keysPressed[key] == true)
+
+        var play = '';
+        if (directionPressed && this.toggleRun) {
+            play = 'Run'
+        } else if (directionPressed) {
+            play = 'Walk'
+        } else {
+            play = 'Idle'
+        }
+
+        if (this.currentAction != play) {
+            const toPlay = this.animationsMap.get(play)
+            const current = this.animationsMap.get(this.currentAction)
+
+            current.fadeOut(fadeDuration)
+            toPlay.reset().fadeIn(fadeDuration).play();
+
+            this.currentAction = play
+        }
+
+        this.mixer.update(delta)
+
+        if (this.currentAction == 'Run' || this.currentAction == 'Walk') {
+            // calculate towards camera direction
+            var angleYCameraDirection = Math.atan2(
+                    (this.camera.position.x - this.model.position.x), 
+                    (this.camera.position.z - this.model.position.z))
+            // diagonal movement angle offset
+            var directionOffset = this.directionOffset(keysPressed)
+
+            // rotate model
+            this.rotateQuarternion.setFromAxisAngle(this.rotateAngle, angleYCameraDirection + directionOffset)
+            this.model.quaternion.rotateTowards(this.rotateQuarternion, 0.2)
+
+            // calculate direction
+            this.camera.getWorldDirection(this.walkDirection)
+            this.walkDirection.y = 0
+            this.walkDirection.normalize()
+            this.walkDirection.applyAxisAngle(this.rotateAngle, directionOffset)
+
+            // run/walk velocity
+            const velocity = this.currentAction == 'Run' ? runVelocity : walkVelocity
+
+            // move model & camera
+            const moveX = this.walkDirection.x * velocity * delta
+            const moveZ = this.walkDirection.z * velocity * delta
+            this.model.position.x += moveX
+            this.model.position.z += moveZ
+            this.updateCameraTarget(moveX, moveZ)
+        }
+    }
+
+    updateCameraTarget(moveX, moveZ){
+        // move camera
+        this.camera.position.x += moveX
+        this.camera.position.z += moveZ
+
+        // update camera target
+        this.cameraTarget.x = this.model.position.x
+        this.cameraTarget.y = this.model.position.y + 1
+        this.cameraTarget.z = this.model.position.z
+        this.orbitControl.target = this.cameraTarget
+    }
+
+    directionOffset(keysPressed){
+        var directionOffset = 0 // w
+
+        if (keysPressed['w']) {
+            if (keysPressed['a']) {
+                directionOffset = Math.PI / 4 // w+a
+            } else if (keysPressed['d']) {
+                directionOffset = - Math.PI / 4 // w+d
+            }
+        } else if (keysPressed['s']) {
+            if (keysPressed['a']) {
+                directionOffset = Math.PI / 4 + Math.PI / 2 // s+a
+            } else if (keysPressed['d']) {
+                directionOffset = -Math.PI / 4 - Math.PI / 2 // s+d
+            } else {
+                directionOffset = Math.PI // s
+            }
+        } else if (keysPressed['a']) {
+            directionOffset = Math.PI / 2 // a
+        } else if (keysPressed['d']) {
+            directionOffset = - Math.PI / 2 // d
+        }
+
+        return directionOffset
+    }
+}
